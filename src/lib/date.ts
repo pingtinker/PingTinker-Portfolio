7449ef12449c5e073bf36d607e1a302b97b9ad9d
pingtinker <sgsringo@gmail.com>
2025-09-16 12:29:30 +0900

    removed all files

--- src/lib/date.ts
+++
@@ -1,8 +0,0 @@
-
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-export const currentDate = ()=>{
-    const datatime = new Date();
-  
-    return (`${datatime.getHours()}:${datatime.getMinutes()} ${month[datatime.getMonth()]} ${datatime.getDate()}, ${datatime.getFullYear()}`)
-    
-  }
